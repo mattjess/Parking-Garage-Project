0dd1e742d14b33a95db49aa0fffcdb7bba5f27ec
dbrink2018 <44406900+dbrink2018@users.noreply.github.com>
2018-11-15 12:13:47 -0500

    Add files via upload

---
+++ Scraper.py
@@ -0,0 +1,8 @@
+import urllib2
+from bs4 import BeautifulSoup
+quote_page = https://v2.aitapps.iu.edu/INPARK_LotCount_V1_Online/IN 
+page = urllib2.urlopen(quote_page) 
+soup = BeautifulSoup(page, 'html.parser')
+name_box = soup.find('h1', attrs={'class': 'Row Fluid'})
+name = name_box.text.strip()
+print name
